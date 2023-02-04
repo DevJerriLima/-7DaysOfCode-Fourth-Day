7dc2ac0137ea456ed838fcdd165c3a38733b01bb
DevJerriLima <123476187+DevJerriLima@users.noreply.github.com>
2023-02-04 14:37:28 -0400

    Implementação do código do arquivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+let numeroDaSorte = 5;
 
+let i = 0;
+while(i < 3){
+    let numeroEscolhido = prompt("Escolha um número de 0 a 10 e ganhe um presente.");
+    
+    if (numeroDaSorte == 5){
+        alert("Parabéns você ganhou um abraço. Sinta-se abraçado.");
+        i == 3;
+    }else{
+        i++;
+    }
+}
