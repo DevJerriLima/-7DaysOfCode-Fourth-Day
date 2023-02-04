8094c75ad9f13ef90d912f3e9229698fcb57d53a
Jerri Lima <dev.jerrilima@gmail.com>
2023-02-04 15:24:09 -0400

    Implementando a resolução com o condicional for

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 let numeroDaSorte = 5;
+let numeroEscolhido = "";
+let acertou = false;
 
-let i = 0;
-while(i < 3){
-    let numeroEscolhido = prompt("Escolha um número de 0 a 10 e ganhe um presente.");
+for(let i = 0; i < 3; i++){
+    numeroEscolhido = prompt("Escolha um número de 0 a 10 e ganhe um presente.");
     
-    if (numeroEscolhido != numeroDaSorte && i ==  2){
-        alert(`Uma pena você não acertou, o número da sorte era ${numeroDaSorte}.`);
-    }else if (numeroEscolhido == numeroDaSorte){
-        alert("Parabéns você ganhou um abraço. Sinta-se abraçado.");
-        i = 2;
-    }else{
-        i++;
+    if (numeroEscolhido == numeroDaSorte){
+        alert(`Parabéns você ganhou um abraço. Sinta-se abraçado. O número da sorte era ${numeroDaSorte}`);
+        acertou = true;
+        break;
     }
-    
+        alert("Errado");
+    }
+    if (!acertou){
+        alert(`Infelizmente você não acertou o número escolhido era ${numeroDaSorte}`);
 }
